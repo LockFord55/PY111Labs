4687df73c9a8e4c40d510cb2f51307ed67704d8d
LockFord55 <lockford55@gmail.com>
2024-07-04 21:51:39 +0300

    Задания №1. Рекурсивный аналог.

--- Зачет/Задача №1.py
+++ Зачет/Задача №1.py
@@ -1,29 +1,43 @@
-def counter(n: int, k: int) -> int:
-    """
-    Функция итеративным способом определяет, кто вылетает на K-слоге считалочки,
-    и находит последнего оставшегося человека из первоначальных N-людей.
+# def counter(n: int, k: int) -> int:
+#     """
+#     Функция итеративным способом определяет, кто вылетает на K-слоге считалочки,
+#     и находит последнего оставшегося человека из первоначальных N-людей.
+#
+#     :param n: Количество человек
+#     :param k: Количество слогов
+#     :return: Номер последнего оставшегося человека
+#     """
+#
+#     people = [i for i in range(1, n + 1)]
+#
+#     count = 0
+#     while len(people) > 1:
+#         for player in people:
+#             count += 1
+#             if count % k == 0:
+#                 if player == people[-1]:
+#                     people.remove(player)
+#                     break
+#                 people.remove(player)
+#                 count += 1
+#                 print(people)
+#
+#     return people[0]
 
-    :param n: Количество человек
-    :param k: Количество слогов
-    :return: Номер последнего оставшегося человека
-    """
+def make_massive(n):
+    massive = [i for i in range(1, n + 1)]
+    return massive
 
-    people = [i for i in range(1, n + 1)]
 
-    count = 0
-    while len(people) > 1:
-        for player in people:
-            count += 1
-            if count % k == 0:
-                if player == people[-1]:
-                    people.remove(player)
-                    break
-                people.remove(player)
-                count += 1
-                print(people)
-
-    return people[0]
+def counter(massive, k):
+    if len(massive) == 2:
+        return massive[1]
+    k -= 1
+    if k > len(massive):
+        k = k - len(massive)
+    massive.pop(k)
+    return counter(massive, k + 3)
 
 
 if __name__ == '__main__':
-    print(counter(7, 3))
+    print(counter(make_massive(7), 3))
